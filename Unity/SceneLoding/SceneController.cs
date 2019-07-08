4b6b6c40e7af5e0f28ac3c179a542a355a15415f
HSMing <1260307197@qq.com>
2019-07-08 19:49:10 +0800

    添加unity场景加载

---
+++ Unity/SceneLoding/SceneController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour
+{
+    public Slider bar;
+    public float minLoadTime;
+
+    private AsyncOperation async;
+    private float time;
+
+    public void LoadScene(int index)
+    {
+        time = 0f;
+        Scene scene = SceneManager.GetSceneByBuildIndex(index);
+        if(scene == null )return;
+        else StartCoroutine(ILoadScene(scene.name));
+    }
+
+    public void LoadScene(string name)
+    {
+        time = 0f;
+        StartCoroutine(ILoadScene(name));
+    }
+
+    IEnumerator ILoadScene(string name)
+    {
+        async = SceneManager.LoadSceneAsync(name);
+        async.allowSceneActivation = false;
+
+        // Continue until the installation is completed
+        while (async.isDone == false)
+        {
+            time += Time.deltaTime;
+
+            bar.value = async.progress;
+            //  if (loadingText != null)
+            //      loadingText.text = "%" + (100 * bar.transform.localScale.x).ToString("####");
+
+            if (async.progress >= 0.9f && time >= minLoadTime * 0.9f)
+            {
+                bar.value = 1;
+                if(time >= minLoadTime)async.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
